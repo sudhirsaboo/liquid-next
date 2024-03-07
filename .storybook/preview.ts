eb4c8a0141d9e72a3c998e89f53693f5c24e0c31
Sudhir Saboo <sudhir.saboo@gmail.com>
2024-03-07 22:25:32 +0530

    [Font] Added font awesome stories

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,8 +1,9 @@
 import type { Preview } from "@storybook/react";
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { fas } from '@fortawesome/free-solid-svg-icons';
-
-library.add(fas);
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { far } from '@fortawesome/free-regular-svg-icons'
+library.add(fab, fas, far)
 
 const preview: Preview = {
   parameters: {
