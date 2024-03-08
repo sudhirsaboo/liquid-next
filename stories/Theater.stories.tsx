68a8bfbfd8bdd4551ccb6666c54dc5751327c4e8
Sudhir Saboo <sudhir.saboo@gmail.com>
2024-03-08 13:49:08 +0530

    [docs] auto generated docs removed and specific docs are added

--- stories/Theater.stories.tsx
+++ stories/Theater.stories.tsx
@@ -1,49 +1,11 @@
 // WIP Not implelemtned
 import React from "react";
-import { ComponentStory, ComponentMeta } from "@storybook/react";
+import type { Meta, StoryObj } from "@storybook/react";
 
 import { Theater } from "../layouts/lib/Theater";
 import { Stage } from "../layouts/lib/Stage";
 import styled from "styled-components";
 
-export default {
-  title: "Liquid/Layout/Theater",
-  component: Theater,
-  argTypes: {
-    left: {},
-    right: {},
-  },
-} as ComponentMeta<typeof Theater>;
-
-const Announcer = styled.div`
-  width: 330px;
-  margin: 10px;
-  border: 1px solid #a9abae;
-`;
-
-const Template: ComponentStory<typeof Theater> = (args) => (
-  <Theater {...args}>
-    <Stage>
-      <div
-        style={{
-          display: "flex",
-          height: "100%",
-          overflow: "auto",
-          justifyContent: "center",
-          flexFlow: "row wrap",
-        }}
-      >
-        {card(25)}
-      </div>
-    </Stage>
-    <Announcer>Announcer here can be a card or a form</Announcer>
-  </Theater>
-);
-
-export const Primary = Template.bind({});
-Primary.args = {
-  leftWidth: 400,
-};
 const list = (count: any) => {
   return Array.apply(null, new Array(count)).map((e, i) => (
     <div
@@ -59,4 +21,46 @@ const card = (count: any) => {
       style={{ margin: 10, width: 120, height: 120, backgroundColor: "yellow" }}
     ></div>
   ));
+};
+
+const meta = {
+  title: "Liquid/Layout/Theater",
+  component: Theater,
+  parameters: {
+    // layout: "centered",
+  },
+  argTypes: {},
+} satisfies Meta<typeof Theater>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+const Announcer = styled.div`
+  width: 330px;
+  margin: 10px;
+  border: 1px solid #a9abae;
+`;
+
+export const Primary: Story = {
+  render: () => (
+    <Theater>
+      <Stage>
+        <div
+          style={{
+            display: "flex",
+            height: "100%",
+            overflow: "auto",
+            justifyContent: "center",
+            flexFlow: "row wrap",
+          }}
+        >
+          {card(25)}
+        </div>
+      </Stage>
+      <Announcer>
+        Announcer here can be a card or a form
+        {list(5)}
+      </Announcer>
+    </Theater>
+  ),
 };
